d6000f9d719a62ee7ad3c0a89e22aad31cdf8e79
luym11 <lym602@126.com>
2019-06-13 20:38:10 +0200

    modified dp_monte.py for small scale test only

--- dp_monte.py
+++ dp_monte.py
@@ -6,39 +6,31 @@ import automaton_dp
 import plot3d_fire
 from maxProbAstar import MaxProbAstar
 from path_evaluation import PathEvaluation
+import pickle
 
-for x in range(10,17):
-        # problem setup
-        p1 = problem_setting.ProblemSetting(target = [[x,18]],_stochastic_environment_flag=1, _setting_num=1)
-        ##########################################################################
-        # Original DP approach
 
-        # monteCarloFireMap read in
-        if(x==10):
-                monteCarloAverageFireMap = [[] for i in range(len(p1.FireMap))]
-                monteCarloFireMap = [[[] for k in range(len(p1.FireMap))] for h in range(p1.monteCarloHorizon)]
-                for k in range(len(p1.FireMap)):
-                        monteCarloAverageFireMap[k] = np.loadtxt('./monteCarloAverage/monteCarloAverageFireMap'+str(k)+'.txt')
-                for h in range(p1.monteCarloHorizon):
-                        for k in range(len(p1.FireMap)):
-                                monteCarloFireMap[h][k] = np.loadtxt('./monteCarlo/monteCarloFireMapTrial'+str(h)+'/monteCarloFireMapAt'+str(k)+'.txt')
+monteCarloFireMap = pickle.load(open('MCFMs3000', "rb"))
+monteCarloAverageFireMap = pickle.load(open('MCAFMs3000', "rb"))
 
-        aDP1 = automaton_dp.AutomatonDP(p1.M,p1.N,p1.T,p1.Map,monteCarloAverageFireMap, monteCarloFireMap, p1.StartPoint,p1.EndPoint,p1.Target,p1.Wall,p1.Fire,Q=4)
-        # try:
-        aDP1.solve()
-        # except:
-        #         print("DP doesn't give a solution")
+testTarget = [[2,3]]
 
-        # plot3d_fire.plot_process(p1.FireMap, aDP1.path, p1.Map, p1.StartPoint, p1.Target, p1.Wall)
+p1 = problem_setting.ProblemSetting(target = testTarget, _stochastic_environment_flag=1, _setting_num=0)
+aDP1 = automaton_dp.AutomatonDP(p1.M,p1.N,p1.T,p1.Map,monteCarloAverageFireMap, monteCarloFireMap, p1.StartPoint,p1.EndPoint,p1.Target,p1.Wall,p1.Fire,Q=4)
+# try:
+aDP1.solve()
+# except:
+#         print("DP doesn't give a solution")
 
-        # maxProbAstar (Dijk) approach
-        # _t = time.time()
-        # _path, searchNodes, lastNode = MaxProbAstar(p1, monteCarloFireMap).astar((8,0),(x,18))
-        # path = list(_path)
-        # elapsed_ = time.time() - _t
-        # pe=PathEvaluation(monteCarloFireMap)
-        # pr=pe.evaluate_path(path)
-        # print('Prob is %f, Computation took: %d seconds' %(pr, elapsed_)) 
-        # print(path)
-        # print()
-        # print()
+# plot3d_fire.plot_process(p1.FireMap, aDP1.path, p1.Map, p1.StartPoint, p1.Target, p1.Wall)
+
+# maxProbAstar (Dijk) approach
+# _t = time.time()
+# _path, searchNodes, lastNode = MaxProbAstar(p1, monteCarloFireMap).astar((8,0),(x,18))
+# path = list(_path)
+# elapsed_ = time.time() - _t
+# pe=PathEvaluation(monteCarloFireMap)
+# pr=pe.evaluate_path(path)
+# print('Prob is %f, Computation took: %d seconds' %(pr, elapsed_)) 
+# print(path)
+# print()
+# print()
